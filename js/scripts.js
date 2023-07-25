38a6ebe1bfdfdde1bdc24af0c183ca1dfb589282
rein-77 <mmadan733@gmail.com>
2023-07-25 16:04:44 +0800

    JS

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,19 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+
+const observer = new IntersectionObserver((entries) =>  {
+    entries.forEach ((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+});
+    
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
